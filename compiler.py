c8b1355a34ce5f4a0e1120d4df3dffab80541a2a
JohnGrubba <nicjontrickshots@gmail.com>
2023-06-01 21:53:05 +0200

    Add Metrics to the Compiler

--- compiler.py
+++ compiler.py
@@ -1,4 +1,4 @@
-import os
+import os, timeit
 import re, argparse
 
 from rewrites.print import parse_print
@@ -25,7 +25,7 @@ variables = {}
 
 def compile_line(line: str, line_no: int):
     if line == "\n":
-        return line
+        return ""
     splitted_line = [x.strip() for x in re.split(':|=', line)]
     for statement in [(re.findall(x[1], line), x[0], statements[x]) for x in statements.keys() if x[0] in line]:
         # Statement description
@@ -78,11 +78,12 @@ def compile_line(line: str, line_no: int):
 
 def main():
     # Parse Arguments
-    parser = argparse.ArgumentParser(description='Create a ArcHydro schema')
-    parser.add_argument('filename', metavar='path', type=str)
-    parser.add_argument('-o', metavar='path', type=str, required=False)
-    parser.add_argument('--dev', action='store_true')
-    parser.add_argument('--run', action='store_true')
+    parser = argparse.ArgumentParser(description='Typhon Compiler')
+    parser.add_argument('filename', metavar='path', type=str, help="Input Filename (.ty)")
+    parser.add_argument('-o', metavar='path', type=str, required=False, help="Output Filename")
+    parser.add_argument('--dev', action='store_true', help="Keeps the temp.c file")
+    parser.add_argument('--run', action='store_true', help="Runs the compiled code after compilation")
+    parser.add_argument('--metrics', action='store_true', help="Prints some metrics about the compiled code")
     args = parser.parse_args()
     try:
         filename = str(args.filename)
@@ -92,7 +93,11 @@ def main():
         output_filename = args.o
     else:
         output_filename = filename.split('.')[0] + ".out"
-    code_lines = open(filename, 'r').readlines()
+    try:
+        code_lines = open(filename, 'r').readlines()
+    except FileNotFoundError:
+        error("Input File not found!")
+    start_time = timeit.default_timer()
     # Detect Possible Libraries
     output = "#include <stdio.h>\n"
     if "bool" in "".join(code_lines):
@@ -107,7 +112,13 @@ def main():
     # Compute Temp Filename
     tempc_filename = "temp.c"
     open(tempc_filename, 'w').write(output)
-    os.system("gcc " + tempc_filename + " -o " + output_filename)
+    os.system("gcc " + tempc_filename + " -o " + output_filename + " -w")
+    # Compilation (Preprocessing) is done
+    if args.metrics:
+        variables_count = len(variables.keys())
+        print(f"Variables used: {variables_count}")
+        print(f"Lines of Code: {len(code_lines)}")
+        print(f"Time: {round(timeit.default_timer() - start_time, 2)}s")
     if not args.dev:
         os.remove(tempc_filename)
     if args.run:
--- temp.c
+++ temp.c
@@ -6,12 +6,10 @@ printf("Enter a Double: ");
 double sos;
 scanf("%lf", &sos);
 printf("%lf\n", sos);
-
 printf("Enter a Float: ");
 float sus;
 scanf("%f", &sus);
 printf("%f\n", sus);
-
 printf("Enter a Integer: ");
 int sees;
 scanf("%d", &sees);
